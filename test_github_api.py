431ac519aa9476711e0e3310dd4f7abdab814d2e
Liam Brew <33580926+Liam-Brew@users.noreply.github.com>
2020-09-30 19:54:51 -0400

    Update test_github_api.py

--- test_github_api.py
+++ test_github_api.py
@@ -9,11 +9,12 @@ class TestGitHubAPI(unittest.TestCase):
     
     def test_get_repos_names(self):
         gh = GitHubAPI("Liam-Brew")
-        self.assertEqual(gh.get_repos(), ['CS-135', 'CS-284', 'GitHubApi567', 'SSW-215', 'SSW-322', 'SSW-345', 'SSW-567', 'Triangle567'], 'CS-135', 'CS-284', 'GitHubApi567', 'SSW-215', 'SSW-322', 'SSW-345', 'SSW-567', 'Triangle567')
+        self.assertEqual(gh.get_repos(), "['CS-135', 'CS-284', 'GitHubApi567', 'SSW-215', 'SSW-322', 'SSW-345', 'SSW-567', 'Triangle567']", 'CS-135', 'CS-284', 'GitHubApi567', 'SSW-215', 'SSW-322', 'SSW-345', 'SSW-567', 'Triangle567')
 
     def test_specific_get_commits(self):
         gh = GitHubAPI("Liam-Brew")
-        self.assertEqual(gh.get_commits()['CS-135'], 6, 'Repo CS-135 should have 6 commits')
+        commits = gh.get_commits()
+        self.assertEqual(commits['CS-135'], 6, 'Repo CS-135 should have 6 commits')
 
     def test_run_status_ok(self):
         gh = GitHubAPI("Liam-Brew")
@@ -21,11 +22,11 @@ class TestGitHubAPI(unittest.TestCase):
 
     def test_404_repo(self):
         gh = GitHubAPI("adwdawwadwwdadwadwwdawadwadddddddddddddddddddd")
-        self.assertEqual(gh.get_repos(), 404, "This user is not on GitHub")
+        self.assertEqual(gh.run(), 404, "This user is not on GitHub")
 
     def test_403(self):
         gh = GitHubAPI("Liam-Brew")
-        for i in range(5):
+        for i in range(10):
             gh.run()
         self.assertEqual(gh.run(), 403, "Too many requests have been performed")
 
